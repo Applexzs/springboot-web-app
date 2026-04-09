2aebb52a7c7495c768fecd018bf29cf48ca25c60
Applexzs <reverseapplemw2@gmail.com>
2026-04-09 17:14:14 -0600

    Agregamos el modelo User, y lo usamos para el controlador y controlador rest

--- src/main/java/com/applexzs/curso/springbootweb/controllers/UserController.java
+++ src/main/java/com/applexzs/curso/springbootweb/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.applexzs.curso.springbootweb.controllers;
 
 
+import com.applexzs.curso.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -11,9 +12,9 @@ public class UserController {
 
     @GetMapping("/details")
     public String details(Map<String, Object> model){
+        User user = new User("Jesus", "Luquin");
         model.put("title", "Hola Mundo Spring Boot");
-        model.put("name", "Jesus");
-        model.put("lastname", "Luquin");
+        model.put("user", user);
         return "details";
     }
 }
--- src/main/java/com/applexzs/curso/springbootweb/controllers/UserRestController.java
+++ src/main/java/com/applexzs/curso/springbootweb/controllers/UserRestController.java
@@ -1,21 +1,25 @@
 package com.applexzs.curso.springbootweb.controllers;
 
 
+import com.applexzs.curso.springbootweb.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserRestController {
 
-    @GetMapping("/details2")
+    @GetMapping(path = "/details")
     public Map<String, Object> details(){
+        User user = new User("Jesus", "Luquin");
         Map<String, Object> body = new HashMap<>();
         body.put("title", "Hola Mundo Spring Boot");
-        body.put("name", "Jesus");
-        body.put("lastname", "Luquin");
+        body.put("user", user);
+
         return body;
     }
 }
