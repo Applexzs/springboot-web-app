ebe0c9f219af646b5453a83d02d940ed0394118d
Applexzs <reverseapplemw2@gmail.com>
2026-04-09 16:29:44 -0600

    Agregamos un hola mundo con spring boot y thymeleaf

---
+++ src/main/java/com/applexzs/curso/springbootweb/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.applexzs.curso.springbootweb.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/details")
+    public String details(Map<String, Object> model){
+        model.put("title", "Hola Mundo Spring Boot");
+        model.put("name", "Jesus");
+        model.put("lastname", "Luquin");
+        return "details";
+    }
+}
