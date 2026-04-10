fcfc3ac4f5f2b76d25647154448fe5253cc82766
Applexzs <reverseapplemw2@gmail.com>
2026-04-09 19:12:08 -0600

    Agregamos una vista de thymeleaf con una tabla de usuarios y en el userController ponemos un metodo que manda el atributo users

--- src/main/java/com/applexzs/curso/springbootweb/controllers/UserController.java
+++ src/main/java/com/applexzs/curso/springbootweb/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.applexzs.curso.springbootweb.controllers;
 
 import com.applexzs.curso.springbootweb.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -17,5 +21,24 @@ public class UserController {
         model.put("title", "Hola Mundo Spring Boot");
         model.put("user", user);
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model){
+
+//        model.addAttribute("users", users);
+        model.addAttribute("title", "Listado de usuarios!");
+
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel(){
+        User user = new User("Pepa", "Gonzales", "pepa@mail.com");
+        User user2 = new User("Lalo", "Perez", "lalo@mail.com");
+        User user3 = new User("Anita", "Roe");
+        List<User> users = Arrays.asList(user, user2, user3);
+        return users;
+
     }
 }
